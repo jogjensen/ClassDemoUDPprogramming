1f76e1e15ce98d28df5807fb37134a284eb47407
Peter Levinsky <pele@easj.dk>
2020-10-29 10:37:01 +0100

    Nu med afsender

--- ClassDemoUDPprogramming/Program.cs
+++ ClassDemoUDPprogramming/Program.cs
@@ -11,6 +11,7 @@ namespace ClassDemoUDPprogramming
 
 
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
--- ClassDemoUDPprogramming/UDPWorker.cs
+++ ClassDemoUDPprogramming/UDPWorker.cs
@@ -1,4 +1,9 @@
-﻿namespace ClassDemoUDPprogramming
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClassDemoUDPprogramming
 {
     internal class UDPWorker
     {
@@ -8,9 +13,32 @@
 
         public void Start()
         {
+
+            UdpClient client = new UdpClient(); // modtage pakker på 7007 port nummer
+
+            byte[] buffer;
+
+                // Sender
+                IPEndPoint modtagerEP = new IPEndPoint(IPAddress.Loopback, 7007);
+                String str = "Hej Peter";
+                byte[] outbuffer = Encoding.UTF8.GetBytes(str.ToCharArray());
+                client.Send(outbuffer, outbuffer.Length, modtagerEP);
+
+
+                // Modtager
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                buffer = client.Receive(ref remoteEP);
+
+                Console.WriteLine($"har modtaget en pakke kommer fra IP {remoteEP.Address} og port {remoteEP.Port}");
+                string incommingstr = Encoding.UTF8.GetString(buffer);
+
+                Console.WriteLine("tekst modtaget = " + incommingstr);
+
+
+
+
+
             
-
-
 
 
 
